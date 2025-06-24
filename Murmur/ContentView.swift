8ece350c248ac09fb3205cae6057ad2a6574c87c
Hyeongi Kim (Wade) <21900215@handong.ac.kr>
2025-06-24 18:05:34 +0900

    init : 프로젝트 초기 세팅

---
+++ Murmur/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Murmur
+//
+//  Created by 김현기 on 6/24/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Murmur/MurmurApp.swift
@@ -0,0 +1,17 @@
+//
+//  MurmurApp.swift
+//  Murmur
+//
+//  Created by 김현기 on 6/24/25.
+//
+
+import SwiftUI
+
+@main
+struct MurmurApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
