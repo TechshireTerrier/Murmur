376a0db3cf702a659aaebe6e18077121e2954965
Yujin Jeon <166476876+romiwaves@users.noreply.github.com>
2025-06-29 13:33:20 +0900

    feat : 오늘의 사연 확인 뷰 제작

---
+++ Murmur/Features/Story/SongStoryView.swift
@@ -0,0 +1,42 @@
+//
+//  SongStoryView.swift
+//  Murmur
+//
+//  Created by gabi on 6/28/25.
+//
+
+import SwiftUI
+
+struct SongStoryView: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("오늘의 사연")
+                .font(.PretendardTitle1Bold)
+                .accessibilityLabel("오늘의 사연")
+                .accessibilityAddTraits(.isHeader)
+            
+            Text("I'd run the risk of losing everything Sell all my things, become nomadic I'd run the risk, and just in case, I might Sell all my things and become the night")
+                .frame(width: 307)
+                .foregroundStyle(Color.gray900)
+                .padding(.vertical, 28)
+                .frame(width: 361, height: 404, alignment: .top)
+                .background(Color.gray50)
+                .clipShape(RoundedRectangle(cornerRadius: 15))
+                .accessibilityLabel("사연 내용")
+                .accessibilityAddTraits(.isStaticText)
+            
+            Button {
+                print("수정하기 버튼 눌림")
+            } label: {
+                Text("수정하기")
+                    .modifier(LongButtonModifier(buttonColor: Color.keyMint))
+            }
+            .accessibilityLabel("수정하기")
+            .accessibilityAddTraits(.isButton)
+        }
+    }
+}
+
+#Preview {
+    SongStoryView()
+}
