f1d867c90385257cbbff52e60eade41a584f15b2
Yujin Jeon <166476876+romiwaves@users.noreply.github.com>
2025-08-10 13:06:59 +0900

    fix: 스크롤바 위치 수정

--- Murmur/Features/Story/DetailStoryView.swift
+++ Murmur/Features/Story/DetailStoryView.swift
@@ -19,6 +19,7 @@ struct DetailStoryView: View {
                 }
                 DetailStoryButtonView()
             }
+            .frame(maxWidth: .infinity)
         }
         .onDisappear {
             musicRecommendationVM.stopPlayback()
